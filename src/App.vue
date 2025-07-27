32ebbb4cc1bc6b5f1ee7308c86f07c52430841ea
John Jones <john@johnjonesfour.com>
2025-07-26 21:34:40 -0400

    added text

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import Calculator from './components/Calculator.vue'
 <template>
   <div class="page">
     <h1>Board Cut Calculator</h1>
+    <p>Paste in a list of cut lengths below and calculate how much stock you need to get the job done!</p>
     <Calculator />
   </div>
 </template>
@@ -17,7 +18,12 @@ import Calculator from './components/Calculator.vue'
 }
 h1 {
   text-align: center;
-  padding: 0 0 0.5em 0;
+  padding: 0 0 0.25em 0;
   margin: 0;
+}
+p {
+  text-align: center;
+  margin: 0 0 1em 0;
+  padding: 0;
 }
 </style>
